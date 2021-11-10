bc87012f540cc23c7230345f5becb54909b9f196
xgeovanedamasceno <xgeovanedamasceno>
2021-11-10 16:15:49 -0300

    Add express

--- nodejs-lab/dio/rest-api-auth/dist/index.js
+++ nodejs-lab/dio/rest-api-auth/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('worked');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/status', (req, res, next) => {
+    res.status(200).send({ foo: 'bar' });
+});
+app.listen(3000, () => {
+    console.log("Running... Port: 3000");
+});
--- nodejs-lab/dio/rest-api-auth/src/index.ts
+++ nodejs-lab/dio/rest-api-auth/src/index.ts
@@ -1,1 +1,11 @@
-console.log('worked')
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' });
+});
+
+app.listen(3000, () => {
+    console.log("Running... Port: 3000");
+});
