c40f0b8b4ff023a3f1b264d9baf855e8b006c325
xgeovanedamasceno <xgeovanedamasceno>
2021-11-11 05:01:06 -0300

    Feat(routeById): This commit add a route by id

--- nodejs-lab/dio/rest-api-auth/src/routes/users.route.ts
+++ nodejs-lab/dio/rest-api-auth/src/routes/users.route.ts
@@ -1,9 +1,18 @@
 import { NextFunction, Router, Request, Response } from "express";
 
 const usersRoute = Router();
+
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{username: 'Paulo'}];
     res.status(200).send({ users })
 });
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    
+    res.status(200).send({ uuid })
+})
+
+
 
 export default usersRoute;
