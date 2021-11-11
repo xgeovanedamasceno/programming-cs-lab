d5303e0493e8141d2c063fe05d588a27df21d075
xgeovanedamasceno <xgeovanedamasceno>
2021-11-11 05:07:58 -0300

    Add StatusCodes lib

--- nodejs-lab/dio/rest-api-auth/src/routes/users.route.ts
+++ nodejs-lab/dio/rest-api-auth/src/routes/users.route.ts
@@ -1,17 +1,20 @@
 import { NextFunction, Router, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{username: 'Paulo'}];
-    res.status(200).send({ users })
+    res.status(StatusCodes.OK).send({ users })
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     
-    res.status(200).send({ uuid })
-})
+    res.status(StatusCodes.OK).send({ uuid })
+});
+
+
 
 
 
