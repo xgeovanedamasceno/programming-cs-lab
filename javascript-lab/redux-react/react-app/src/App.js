a7497f3158fc7f13ae58771847e18bf25792bee9
xgeovanedamasceno <xgeovanedamasceno>
2021-11-17 11:31:48 -0300

    Add middlewares

--- javascript-lab/redux-react/react-app/src/App.js
+++ javascript-lab/redux-react/react-app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "./store/counter";
+import { close, open } from "./store/modal";
 
 function App() {
   const stateCounter = useSelector(state => state.counter.total);
+  const stateModal = useSelector(state => state.modal)
   console.log(stateCounter)
   const dispatch = useDispatch();
 
@@ -11,9 +13,12 @@ function App() {
   return (
     <div>
      <h1>React Redux App</h1>
-     <h2>Total: {stateCounter}</h2>
-     <button onClick={() => dispatch(increment())}>Increment</button>
-     <button onClick={() => dispatch(decrement())}>Decrement</button>
+     {stateModal && <h2>Total: {stateCounter}</h2>}
+     
+     <button onClick={ () => dispatch(increment()) }>Increment</button>
+     <button onClick={ () => dispatch(decrement()) }>Decrement</button>
+     <button onClick={ () => dispatch(open()) }>Open Modal</button>
+     <button onClick={ () => dispatch(close()) }>Close Modal</button>
 
     </div>
   );
--- javascript-lab/redux-react/react-app/src/store/configureStore.js
+++ javascript-lab/redux-react/react-app/src/store/configureStore.js
@@ -1,10 +1,15 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import counter from './counter';
 import modal from './modal';
-
+import logger from './middlewares/logger'
 
 
 const reducer = combineReducers({counter, modal })
-const store = configureStore({ reducer });
+const store = configureStore({ 
+  reducer, 
+  middleware: (getDefaultMiddleware) => [ 
+    ...getDefaultMiddleware(), logger
+  ]
+});
 
 export default store;
