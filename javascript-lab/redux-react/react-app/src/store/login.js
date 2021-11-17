fcc1c9cd5c2bae1d690390b42d87ba27b410c064
xgeovanedamasceno <xgeovanedamasceno>
2021-11-17 15:59:53 -0300

    Add createAsyncSlice.js

--- javascript-lab/redux-react/react-app/src/store/login.js
+++ javascript-lab/redux-react/react-app/src/store/login.js
@@ -1,90 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
+import { combineReducers } from "@reduxjs/toolkit";
 
-const slice = createSlice({
-    name: 'login',
-    initialState: {
-        token: {
-            loading: false,
-            data: null,
-            error: null
-        },
-        user: {
-            loading: false,
-            data: null,
-            error: null
-        }
-        
-    },
-    reducers: {
-        fetchTokenStarted(state) {
-            state.token.loading = true;
-        },
-        fetchTokenSuccess(state, action) {
-            state.token.loading = false;
-            state.token.data = action.payload;
-            state.token.error = null;
-        },
-        fetchTokenError(state, action) {
-            state.token.loading = false;
-            state.token.error = action.payload;
-        },
-        fetchUserStarted(state) {
-            state.user.loading = true;
-        },
-        fetchUserSuccess(state, action) {
-            state.user.loading = false;
-            state.user.data = action.payload;
-            state.user.error = null;
-        },
-        fetchUserError(state, action) {
-            state.user.loading = false;
-            state.user.error = action.payload;
-        },
-    },
-});
+import createAsyncSlice from "./helper/createAsyncSlice";
 
-const { 
-    fetchTokenStarted, 
-    fetchTokenSuccess, 
-    fetchTokenError,
-    fetchUserStarted,
-    fetchUserSuccess,
-    fetchUserError
-} = slice.actions;
-
-export const fetchToken = (user) => async (dispatch) => {
-    try {
-        dispatch(fetchTokenStarted());
-        const response = await fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+const token = createAsyncSlice({
+    name: 'token',
+    fetchConfig: (userPayload) => ({
+        url: 'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
+        options: {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json' 
             },
-            body: JSON.stringify(user),
-        });
-        const data = await response.json();
-        return dispatch(fetchTokenSuccess(data));
-    } catch (error) {
-        return dispatch(fetchTokenError(error.message))
-    }
-}
+            body: JSON.stringify(userPayload),
+        },
+    })
+});
 
-export const fetchUser = (token) => async (dispatch) => {
-    try {
-        dispatch(fetchUserStarted());
-        const response = await fetch('https://dogsapi.origamid.dev/json/api/user', {
+const user = createAsyncSlice({
+    name: 'user',
+    fetchConfig: (tokenPayload) => ({
+        url: 'https://dogsapi.origamid.dev/json/api/user',
+        options: {
             method: 'GET',
             headers: {
-                'Authorization': 'Bearer '  + token,
+                'Authorization': 'Bearer '  + tokenPayload,
             },
-        });
-        const data = await response.json();
-        return dispatch(fetchUserSuccess(data));
-    } catch (error) {
-        return dispatch(fetchUserError(error.message))
-    }
-}
+        },
+    })
+});
+
+
+const reducer = combineReducers({ token: token.reducer, user: user.reducer})
+
+const fetchToken = token.asyncAction;
+const fetchUser = user.asyncAction;
 
 export const login = (user) => async (dispatch) => {
     try {
@@ -97,4 +46,12 @@ export const login = (user) => async (dispatch) => {
     }
 }
 
-export default slice.reducer;
+export default reducer;
+
+
+
+
+
+
+
+
