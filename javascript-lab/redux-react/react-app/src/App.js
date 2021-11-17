3615f9b61022254b71c265064c145aa9791be2a5
xgeovanedamasceno <xgeovanedamasceno>
2021-11-17 09:40:38 -0300

    This commid add useSelect and useDispatch hooks

--- javascript-lab/redux-react/react-app/src/App.js
+++ javascript-lab/redux-react/react-app/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 
 function App() {
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
     <div>
      <h1>React Redux App</h1>
+     <h2>Total: {state}</h2>
+     <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
     </div>
   );
 }
--- javascript-lab/redux-react/react-app/src/index.js
+++ javascript-lab/redux-react/react-app/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+
+
+
+function reducer(state = 0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION())
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+     <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
