106854da72f1c74b19a2e1196ec11e5ac23a32d3
xgeovanedamasceno <xgeovanedamasceno>
2021-11-17 23:35:41 -0300

    This commit add prepare method

--- javascript-lab/redux-react/react-app/src/store/counter.js
+++ javascript-lab/redux-react/react-app/src/store/counter.js
@@ -3,19 +3,21 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const slice = createSlice({
     name: 'counter',
-    initialState: {
-        total: 0
-    },
+    initialState: 0,
     reducers: {
-        increment(state) {
+       /*  increment(state) {
             state.total++;
         },
         decrement(state) {
             state.total--;
+        }, */
+        sum: {
+            reducer: (state, action) => state + action.payload,
+            prepare: (payload) => ({ payload, meta: 'local'})
         }
     }
 })
 
 
-export const { increment, decrement } = slice.actions;
+export const { increment, decrement, sum } = slice.actions;
 export default slice.reducer;
