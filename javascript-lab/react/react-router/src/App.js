924488850c2836935ddca79bcd29788d19e3da04
xgeovanedamasceno <xgeovanedamasceno>
2021-11-22 10:47:43 -0300

    react-router

--- javascript-lab/react/react-router/src/App.js
+++ javascript-lab/react/react-router/src/App.js
@@ -1,29 +1,12 @@
 import './App.css'
 
 import React from 'react';
-import Slide from './Slide';
-
 
 function App() {
-  
-  const slides = [
-    {
-      id: "slide1",
-      text: "Slide 1"
-    },
-    {
-      id: "slide2",
-      text: "Slide 2"
-    },
-    {
-      id: "slide3",
-      text: "Slide 3"
-    }
-  ]
-  
+    
   return (
     <div>
-      <Slide slides={slides}/>
+      
     </div>
   );
 }
