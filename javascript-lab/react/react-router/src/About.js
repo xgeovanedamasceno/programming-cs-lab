1c6d71ac9565c6a478d430646b285a6ac7636f82
xgeovanedamasceno <xgeovanedamasceno>
2021-11-23 10:52:49 -0300

    Add Head Compononent

--- javascript-lab/react/react-router/src/About.js
+++ javascript-lab/react/react-router/src/About.js
@@ -6,7 +6,6 @@ const About = () => {
     return (
         <div>
             <Head title="About" description="About Page"/>
-
             <h1>About Us</h1>
             <p>React Router Dom</p>
         </div>
