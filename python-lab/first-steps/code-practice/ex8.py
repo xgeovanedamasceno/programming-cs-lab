815715b0c6d98b4f70391aa40d8db41000cf1d5e
xgeovanedamasceno <xgeovanedamasceno>
2021-11-22 21:19:50 -0300

    Add ex8.py

--- python-lab/first-steps/code-practice/ex8.py
+++ python-lab/first-steps/code-practice/ex8.py
@@ -1,9 +1,9 @@
-formatter = "{} {} {} {}"
+formatter = "{} {} {} {}" #create a string with 4 arguments
 
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
-print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(formatter, formatter, formatter, formatter)) # passing the string as an argument
 print(formatter.format(
     "Who knows",
     "what the future holds?",
