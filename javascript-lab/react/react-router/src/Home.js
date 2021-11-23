12b45dca8aa01559e67a139dd467f9f3a94a313b
xgeovanedamasceno <xgeovanedamasceno>
2021-11-23 10:48:51 -0300

    Add Head Component

--- javascript-lab/react/react-router/src/Home.js
+++ javascript-lab/react/react-router/src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Head from './Head';
 
 const Home = () => {
     return (
         <div>
+            <Head title="Home" description="Home Page"/>
             <h1>Home</h1>
             <p>Hello there. Welcome!</p>
             <Link to="product/notebook">Notebook</Link>
