5a56464968bab28951985cf07ec98a8710a40b80
xgeovanedamasceno <xgeovanedamasceno>
2021-11-29 10:30:55 -0300

    Add express lib and set server

--- javascript-lab/nodejs/dio/rest-api-auth/dist/index.js
+++ javascript-lab/nodejs/dio/rest-api-auth/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Hello Node');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/status', (req, res, next) => {
+    res.status(200).send({ greettings: 'Hello There!' });
+});
+app.listen(3000, () => {
+    console.log('Hello Node! App running... Port 3000');
+});
--- javascript-lab/nodejs/dio/rest-api-auth/src/index.ts
+++ javascript-lab/nodejs/dio/rest-api-auth/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello Node')
+import express, {Request, Response, NextFunction} from 'express';
+
+// app's instance
+const app = express(); // lib to manage routes
+
+// setting route and response data
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ greettings: 'Hello There!' });
+});
+
+// setting door
+app.listen(3000, () => {
+    console.log('Hello Node! App running... Port 3000');
+})
