c936182b7036e19b8c84bade514f22d534927937
xgeovanedamasceno <xgeovanedamasceno>
2021-11-30 16:20:00 -0300

    Delete url-shortener project

--- javascript-lab/nodejs/dio/url-shortener-dio/src/config/Constants.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    API_URL: "http://localhost:5000",
-    MONGO_CONNECTION: 
-        "mongodb+srv://xgadmin:<admin>@url-shortner-dio.o2l4r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    
-}
