d6303f7635582436d63376798ece3e2ffc712067
xgeovanedamasceno <xgeovanedamasceno>
2021-12-21 17:21:01 -0300

    Add ex16c - extra exercise

--- hard-way-book/ex16.py
+++ hard-way-book/ex16.py
@@ -12,6 +12,10 @@ print("Opening the file...")
 target = open(filename, 'w')
 
 print("Truncating the file. Goodbye!")
+"""
+this is not necessary because open a file with mode 'w' trucating the file if
+it already exists
+"""
 target.truncate()
 
 
@@ -23,12 +27,7 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"{line1} \n {line2} \n {line3}")
 
 print("And finnaly, we close it.")
 target.close()
--- hard-way-book/ex16b.py
+++ hard-way-book/ex16b.py
@@ -2,6 +2,19 @@ from sys import argv
 
 filename = argv[1]
 
-txt = open(filename)
+txt = open(filename, 'a')
 
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+txt.write(f"{line1} \n {line2} \n {line3}")
+txt.write("\n")
+
+txt.close()
+
+
+txt = open(filename)
 print(txt.read())
+
+txt.close()
