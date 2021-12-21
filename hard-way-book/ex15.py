f33f75bd4e9f0e4e7316af8e9504cc6dd6498bd0
xgeovanedamasceno <xgeovanedamasceno>
2021-12-21 15:47:43 -0300

    Add foo.py and add comments to ex15 script

--- hard-way-book/ex15.py
+++ hard-way-book/ex15.py
@@ -1,15 +1,46 @@
+"""
+Importing argv from sys module
+
+sys module provides access to some variable used or maintained by the interpreter
+and to functions that interact strongly with the interpreter.
+
+argv é um 'array de arguments' passed by commando line to a Python script.
+argv[0] is the script name
+"""
 from sys import argv
 
+
+"""
+script and filename are variables. They will receive the arguments passed by comand line
+script = argv[0] (script name: ex15.py)
+filename = argv[1] input user
+"""
 script, filename = argv
 
+"""
+open built-in python function
+open a file, return a corresponding file object and assigned to txt variable
+"""
 txt = open(filename)
 
 print(f"Here's your file {filename}:")
+print(f"It will open throught out this {script} script")
+
+"""
+read  txt value and print it
+"""
 print(txt.read())
 
 print("Type the filename again:")
-file_again = input("> ")
+"""
+read input keyboard and assigned to file_again variable
+"""
+file_again = input("> ") 
 
+
+"""
+open file and print it value
+"""
 txt_again = open(file_again
 )
 print(txt_again.read())
