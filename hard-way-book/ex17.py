8bdb97f206f6cbbb7cc984b7bcbc8af1dc3ab01d
xgeovanedamasceno <xgeovanedamasceno>
2021-12-22 15:45:11 -0300

    Refactor ex17 to python3

--- hard-way-book/ex17.py
+++ hard-way-book/ex17.py
@@ -3,16 +3,16 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print("Copying from %s to %s" % (from_file, to_file))
+print(f"Copying from {from_file} to {to_file}")
 
 in_file = open(from_file)
 indata = in_file.read()
 
-print("The input file is %d bytes long" % len(indata))
+print(f"The input file is {len(indata)} bytes long")
 
-print("Does the output file exist? %r" % exists(to_file))
+print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit ENTER to continue, CTRL-C to abort.")
-raw_input()
+input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
