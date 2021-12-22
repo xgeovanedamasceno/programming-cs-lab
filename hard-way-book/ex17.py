93268d03e206d2f405975d7e41d7d371ac7cbe57
xgeovanedamasceno <xgeovanedamasceno>
2021-12-22 16:06:16 -0300

    Refactor ex17 - chaining methods

--- hard-way-book/ex17.py
+++ hard-way-book/ex17.py
@@ -5,19 +5,20 @@ script, from_file, to_file = argv
 
 print(f"Copying from {from_file} to {to_file}")
 
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} bytes long")
 
 print(f"Does the output file exist? {exists(to_file)}")
-print("Ready, hit ENTER to continue, CTRL-C to abort.")
-input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
 print("Alright, all done.")
 
-out_file.close()
-in_file.close()
+out_file.close() 
+
+"""
+It is important to close opened files because a open file is locked for access for
+another program.
+"""
