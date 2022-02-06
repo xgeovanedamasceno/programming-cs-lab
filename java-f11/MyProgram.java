0dece2e4f57f11a7b66c24886207e47dc0239cb7
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-02-06 12:56:47 -0300

    add my first java program

---
+++ java-f11/MyProgram.java
@@ -0,0 +1,7 @@
+class MyProgram {
+  public static void main(String[] args) {
+    System.out.println("Hello there!");
+    System.out.println("My first Java Application!");
+    System.out.println("'Better late than never!'\n...as my father used to say!");
+  }
+}
