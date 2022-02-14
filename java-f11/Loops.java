cff5469a939793ff1971b92c91e22985726ff046
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-02-14 17:06:06 -0300

    add ex cap3

---
+++ java-f11/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i > 50 && i < 60) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
