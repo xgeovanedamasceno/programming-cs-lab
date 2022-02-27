9a5556c713885e3bc1ccdf176cfba20231a6fc37
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-02-27 18:33:26 -0300

    refactor ex04 - change factorial variable type to long

--- java-lab/java-f11/Factorial.java
+++ java-lab/java-f11/Factorial.java
@@ -1,8 +1,7 @@
 public class Factorial {
   public static void main(String[] args) {
-    int factorial = 1;
-    System.out.println("Factorial of 0 is " + factorial);
-    for (int n = 1; n <= 10; n++) {
+    long factorial = 1;
+    for (int n = 1; n <= 20; n++) {
       factorial *= n;
       System.out.println("Factorial of " + n + " is " + factorial);
     }
