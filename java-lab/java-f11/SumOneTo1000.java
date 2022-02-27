89628c824ead549b50bd509a9d7984ec7c6c0e1b
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-02-27 17:14:38 -0300

    refactor ex02 - sum 1 to 1000

--- java-lab/java-f11/SumOneTo1000.java
+++ java-lab/java-f11/SumOneTo1000.java
@@ -1,7 +1,7 @@
 public class SumOneTo1000 {
   public static void main(String[] args) {
     int sum = 0;
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 1; i <= 1000; i++) {
       sum += i;
       System.out.println(sum);
 
