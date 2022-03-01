62c0437faabbc13505e47fdc992c995b04003999
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-01 18:50:16 -0300

    alter sysout

--- java-lab/java-f11/EvenOrOdd.java
+++ java-lab/java-f11/EvenOrOdd.java
@@ -5,10 +5,10 @@ public class EvenOrOdd {
     while (x != 1) {
       if (x % 2 == 0) {
         x = x / 2;
-        System.out.print(" -> " + x);
+        System.out.print(x + " -> ");
       } else {
         x = 3 * x + 1;
-        System.out.print(" -> " + x);
+        System.out.print(x + " -> ");
       }
     }
   }
