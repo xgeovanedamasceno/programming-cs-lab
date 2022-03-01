3958738ede9bf11c4db5460275ce7013e7ab0fa7
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-01 20:12:44 -0300

    add pyramid of numbers

---
+++ java-lab/java-f11/PyramidoFNumbers.java
@@ -0,0 +1,13 @@
+public class PyramidoFNumbers {
+  public static void main(String[] args) {
+    for (int i = 1; i < 5; i++) {
+      for (int j = i; j < i + 1; j++) {
+        System.out.println("valor de i " + i);
+        int mult = 2;
+        for (int k = j; k < j + 1; k++) {
+
+        }
+      }
+    }
+  }
+}
