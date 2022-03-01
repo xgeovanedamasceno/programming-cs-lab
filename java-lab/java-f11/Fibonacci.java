b897f7425b09f4244ad48a807658869f2a1b64df
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-01 18:37:55 -0300

    fix logic code - fibonacci

--- java-lab/java-f11/Fibonacci.java
+++ java-lab/java-f11/Fibonacci.java
@@ -1,30 +1,13 @@
 public class Fibonacci {
   public static void main(String[] args) {
     int fib = 0;
-    int last  = 0;
-    int penultimate = 0;
-    for (int i = 1; fib < 22; i++) {
-      if (i == 0) {
-        fib = last - penultimate;
-        System.out.println("valor de i " + i);
-        System.out.println(fib);
-        continue;
-      }
-      if (i == 1) {
-        last = i;
-        fib = last + penultimate;
-        System.out.println("valor de i " + i);
-        System.out.println(fib);
-        continue;
-      }
-      if (i == 2) {
-        fib = last + penultimate;
-        penultimate = last;
-      }
-      System.out.println("valor de i eh" + i);
-      fib = last + penultimate;
+    int last = 0;
+    int current = 1;
+    while (current < 22) {
+      fib = last + current;
       System.out.println(fib);
-      last = fib;
+      last = current;
+      current = fib;
     }
   }
 }
