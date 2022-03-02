2eb2ea2bbd6fc0fb3db315a390a2008e47d75262
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-02 14:44:04 -0300

    add challenge 3.14

---
+++ java-lab/java-f11/FibonacciNew.java
@@ -0,0 +1,12 @@
+public class FibonacciNew {
+  public static void main(String[] args) {
+    int last = 0;
+    int current = 1;
+    System.out.print(last + " " + current + " ");
+    while (current < 100) {
+      current = last + current;
+      System.out.print(current + " ");
+      last = current - last;
+    }
+  }
+}
