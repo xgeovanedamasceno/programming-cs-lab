6904f0528d9f5abe14e552113ef6a1ddfe173262
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-02 16:37:15 -0300

    add account class

---
+++ java-lab/java-f11/oop/capfour/Account.java
@@ -0,0 +1,30 @@
+package oop.capfour;
+
+public class Account {
+  String holder;
+  int number;
+  String branch;
+  double balance;
+  String openingDate;
+
+  public void withDrawMoney(double amount) {
+    this.balance -= amount;
+  }
+
+  public void cashDeposit(double amount) {
+    this.balance += amount;
+  }
+
+  public double calculateFinancialIncome() {
+    return this.balance * 0.1;
+  }
+
+  public String recoverDataForPrinting() {
+    String data = "Number Account: " + this.number + "\n" +
+           "Opening Date: " + this.openingDate + "\n" +
+           "Holder: " + this.holder + "\n" +
+           "Balance: " + this.balance + "\n";
+    return data;
+
+  }
+}
