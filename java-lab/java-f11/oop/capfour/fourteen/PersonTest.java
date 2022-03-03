5c643775ff507a7921f709d48d66cc53a24659c7
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-03 17:10:33 -0300

    add person class

---
+++ java-lab/java-f11/oop/capfour/fourteen/PersonTest.java
@@ -0,0 +1,14 @@
+package oop.capfour.fourteen;
+
+public class PersonTest {
+  public static void main(String[] args) {
+    Person p1 = new Person("Daniel", 23);
+
+    p1.showIdentity();
+
+    p1.makesBirthday();
+
+    p1.showIdentity();
+
+  }
+}
