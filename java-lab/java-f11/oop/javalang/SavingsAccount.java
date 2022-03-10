91999c8243d6154f39595ca75eae5a64d4f05a10
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-10 11:10:33 -0300

    add javalang pkg

---
+++ java-lab/java-f11/oop/javalang/SavingsAccount.java
@@ -0,0 +1,17 @@
+package oop.javalang;
+
+public class SavingsAccount extends Account {
+
+  public SavingsAccount(Data data, String holder, String branch) {
+    super(data, holder, branch);
+  }
+
+  public SavingsAccount(Data data) {
+    super(data);
+  }
+
+  @Override
+  public String getType() {
+    return "Saving Account";
+  }
+}
