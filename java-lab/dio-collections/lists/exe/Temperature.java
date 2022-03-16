4ba7390ae06834d5c60e84993f8a435fc42abe23
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-16 16:18:04 -0300

    add list of temperatures

---
+++ java-lab/dio-collections/lists/exe/Temperature.java
@@ -0,0 +1,25 @@
+package diocollection.lists.exe;
+
+public class Temperature {
+    Double temperature;
+    String month;
+
+    Temperature(Double temp, String month) {
+        this.temperature = temp;
+        this.month = month;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    @Override
+    public String toString() {
+        return "Month: " + month + " " +
+                "Temperature: " + temperature;
+    }
+}
