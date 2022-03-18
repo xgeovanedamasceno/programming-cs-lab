a3663e4c06a12a8dee29b5d065a9c1440e1e0bd0
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-03-18 13:41:02 -0300

    add ex34

---
+++ python-lab/python-hard-way-book/ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3', "peacock", "kangaroo", "whale", "platypus"]
+
+print(animals[1])
+print(animals[3-1])
+print(animals[1-1])
+print(animals[3])
+print(animals[5-1])
+print(animals[2])
+print(animals[6-1])
+print(animals[4])
