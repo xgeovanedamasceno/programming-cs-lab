464595723bb0f3c39be42ef283db6a65719eb4f3
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-04-11 17:23:05 -0300

    add an example of comparable

---
+++ java-lab/dio-collections/comparers/TestComparable.java
@@ -0,0 +1,23 @@
+package diocollection.comparers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestComparable {
+    public static void main(String[] args) {
+        List<Movie> listMovies = new ArrayList<>();
+        listMovies.add(new Movie("Toy Story", 2010, 9.4));
+        listMovies.add(new Movie("Star Wars", 1977, 9.6));
+        listMovies.add(new Movie("Empire of Love", 1980, 7.4));
+        listMovies.add(new Movie("You are alone", 1983, 4.8));
+
+        System.out.println(listMovies);
+
+        Collections.sort(listMovies);
+
+        System.out.println("After sorting:");
+
+        listMovies.forEach(movie -> System.out.println(movie));
+    }
+}
